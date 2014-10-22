4215f8e073eb38f8b663b5f12e02a83142273dc2
Hervé Audren <h.audren@gmail.com>
2014-10-22 17:43:54 +0900

    Creating a group now sets its background

--- kengui.py
+++ kengui.py
@@ -10,14 +10,19 @@ class Kengui(object):
     self.master = master
     self.kenken = kenken.Kenken(size)
     self.labels = [tk.Label(self.master, highlightbackground='black', highlightthickness=1,\
-        text=str(i)) for i in range(size**2)]
+        height=2, width=3, text=" ") for i in range(size**2)]
     self.selected = []
+    self.color_index = 0
+    self.colors = ['#e8e2d5', '#fbda7e', '#e2e2e2', '#9ee7ff', '#99accb', '#afaba2', '#c0a96b', '#d7d7d7', '#00b4ff', '#cdd8ea', '#b5aa94', '#cba81a', '#00ddff', '#97d5ff', '#a7abb4', '#e1d0aa', '#ffd91c', '#00c7ff', '#7daeec']
+
+    self.default_bg = root.cget('bg')
 
     master.bind("<Escape>", self.clear_selection)
     master.bind("<Return>", self.create_region)
     master.bind("<F1>", self.solve)
 
     for i, label in enumerate(self.labels):
+      label.grid_propagate(0)
       label.grid(row=i/size, column=i%size)
       label.bind("<Button-1>", self.add_selection)
 
@@ -25,6 +30,7 @@ class Kengui(object):
     index = -1
     for i, label in enumerate(self.labels):
       if label == event.widget:
+        label['background'] = 'white'
         index = i
         break
 
@@ -32,8 +38,14 @@ class Kengui(object):
     print self.selected
 
   def clear_selection(self, event):
+    for index in self.selected:
+      self.clear_label_format(index)
     self.selected = []
     print "Cleared selection"
+
+  def clear_label_format(self, index):
+    if self.labels[index]['background'] == 'white':
+      self.labels[index]['background'] = self.default_bg
 
   def create_region(self, event):
     if not self.selected:
@@ -42,6 +54,13 @@ class Kengui(object):
     op = tkSimpleDialog.askstring("Operation", "Please enter one of +-/*")
     value = tkSimpleDialog.askinteger("Total", "Please enter desired result")
     self.kenken.add_region([(i/6, i%6) for i in self.selected], op, value)
+
+    for index in self.selected:
+      self.labels[index]['background'] = self.colors[self.color_index]
+
+    self.color_index += 1
+    self.color_index %= len(self.colors)
+
     self.clear_selection(None)
 
   def solve(self, event):
@@ -51,5 +70,5 @@ class Kengui(object):
 
 if __name__ == '__main__':
   root = tk.Tk()
-  gui = Kengui(root, 2)
+  gui = Kengui(root, 6)
   root.mainloop()
